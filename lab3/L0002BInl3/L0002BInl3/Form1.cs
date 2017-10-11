63bf987588c0dfadaaf44d868a5beec7a4d08202
Niklas Brynolf <niklas.brynolf@gmail.com>
2017-10-11 18:00:23 +0200

    lab 3 done

---
+++ lab3/L0002BInl3/L0002BInl3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L0002BInl3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PersonCalculator person = new PersonCalculator(textBox1.Text,textBox2.Text, textBox3.Text);
+
+            if (person.CalculateSocialSecurityNumber())
+            {
+                groupBox1.Text = "OK";
+                label5.Text = "First name: " + person.GetFirstName() + "\n" + "Surname: " + person.GetSurName() + "\n" + "Social security number: " + person.GetSocialSecurityNumber() + "\n" + "Sex: " + person.GetSex();
+            }
+            else {
+                groupBox1.Text = "Failure";
+                label5.Text = "Invalid Social security number: " + "\n" + person.GetSocialSecurityNumber();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            label5.Text = "";
+            groupBox1.Text = "Pending";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ lab3/L0002BInl3/L0002BInl3/PersonCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L0002BInl3
+{
+    class PersonCalculator
+    {
+        private string FirstName ;
+        private string SurName;
+        private string SocialSecurityNumber;
+        private int Buffer;
+        private int Holder;
+        private int TempHolder;
+
+        public PersonCalculator(string FirstName, string SurName, string SocialSecurityNumber)
+        {
+            this.FirstName = FirstName;
+            this.SurName = SurName;
+            this.SocialSecurityNumber = SocialSecurityNumber;
+        }
+
+        public string GetSex()
+        { 
+            if(this.SocialSecurityNumber.Length != 10 ){
+                return "social security number is wrong,  enter a 10 digit number";
+            }
+            else if(this.SocialSecurityNumber[8] % 2 == 0) {
+                return "Woman";
+            }
+            return "Man";
+        }
+
+        public string GetFirstName() {
+            return this.FirstName;
+        }
+
+        public string GetSurName() {
+            return this.SurName;
+        }
+
+        public string GetSocialSecurityNumber() {
+            return this.SocialSecurityNumber;
+        }
+
+        public Boolean CalculateSocialSecurityNumber() {
+            if (this.SocialSecurityNumber.Length != 10) {
+                return false;
+            }
+            for (int PlaceCounter = 0; PlaceCounter < this.SocialSecurityNumber.Length; PlaceCounter++ )
+            {
+                this.Holder = (int)Char.GetNumericValue(this.SocialSecurityNumber[PlaceCounter]);
+                if (PlaceCounter % 2 == 0)
+                {
+                    this.Holder = this.Holder * 2;
+                    if (this.Holder >= 10) {
+                        this.TempHolder = this.Holder % 10;
+                        this.Holder = this.Holder / 10;
+                        this.Holder = this.Holder + this.TempHolder;
+ 
+                    }
+                }
+                this.Buffer = this.Buffer + this.Holder;
+            }
+            if(this.Buffer % 10 != 0){
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
